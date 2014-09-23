36d05c821faca11bc5e9980228e82a5b22e2ff2b
Mridul <mridul@WAYEO.(none)>
2014-09-23 19:02:05 +0530

    Initial Commit

---
+++ content/General/hello-world.md
@@ -0,0 +1,14 @@
+Title: Hello World
+Date: 2014-09-03 07:31
+Tags: hello, world
+Category: General
+Slug: hello-world
+Author: Mridul Malpotra
+Summary: Hello World!
+
+This blog will record all the progress that I make on the project.
+All updates will be posted here and synced with github pages.
+
+The project that I will be working on is [NILMTK](https://github.com/nilmtk/nilmtk)
+
+#### _Mridul_
---
+++ pelicanconf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Mridul Malpotra'
+SITENAME = u'NILMTK Blog - Mridul Malpotra'
+SITEURL = ''
+
+PATH = 'content'
+THEME = "/home/mridul/nilmtk/pelican/pelican-themes/fresh"
+
+TIMEZONE = 'Asia/Kolkata'
+DISPLAY_PAGES_ON_MENU = True
+DEFAULT_LANG = u'en'
+
+#THEME = '/home/mridul/nilmtk/pelican/pelican-themes/voidy-bootstrap/'
+SITESUBTITLE ='Independent Project - Monsoon 2014'
+STYLESHEETS = ("pygment.css", "voidybootstrap.css",)
+CUSTOM_ARTICLE_SHARING = "sharing.html"
+CUSTOM_ARTICLE_SCRIPTS = "sharing_scripts.html"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('NILMTK','https://github.com/nilmtk/nilmtk'),
+		('Pelican', 'http://getpelican.com/'),
+		('Python', 'http://python.org/'),
+		)
+
+# Social widget
+SOCIAL = (('My Github', 'https://github.com/mridulmalpotra'),
+	('Google+', 'https://plus.google.com/+MridulMalpotra'),
+	('Facebook', 'https://www.facebook.com/mridul.malpotra')
+	)
+
+DEFAULT_PAGINATION = 10
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATH = './plugins'
+PLUGINS = ['ipynb']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
